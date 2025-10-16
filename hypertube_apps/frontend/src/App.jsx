697f999124e5aedaf619c9df787331bae6518195
Benjamin Gaertner <benjaming@outlook.fr>
2025-10-16 16:52:15 +0200

    merge with vincent

--- hypertube_apps/frontend/src/App.jsx
+++ hypertube_apps/frontend/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const socket = new WebSocket("ws://127.0.0.1:8000/ws");
+
+    socket.onopen = () => {
+      console.log("Connected !");
+    };
+    socket.onmessage = (event) => console.log("Server:", event.data);
+    return () => socket.close();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+		<div style={{ textAlign: 'center', marginTop: '20%' }}>
+			<h1>OK.Tube !</h1>
+		</div>
     </>
   )
 }
