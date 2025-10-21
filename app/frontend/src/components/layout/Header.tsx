9ba7ed22f5ee82864da6e3419015b7b76c6f821c
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-10-21 14:13:21 +0200

    button + image button for profile

--- app/frontend/src/components/layout/Header.tsx
+++ app/frontend/src/components/layout/Header.tsx
@@ -1,11 +1,17 @@
 import styles from "./Header.module.css";
+import ImageButton from "../ui/ImageButton.tsx";
 
 export default function Header() {
 	return (
 		<header className={styles.container}>
 			<h1 id={styles.title}>Ok.Tube</h1>
 			<input type="text" id="Search" name="Search" placeholder="Search" />
-			<button>Profile</button>
+			<ImageButton
+				icon="src/assets/test/pp.png"
+				onClick={() => console.log("PPPPP")}
+				size="medium"
+				style={{ borderRadius: "50%" }}
+			/>
 		</header>
-	); // TODO: REMPLACER BOUTON PAR COMPONENT REACT
+	);
 }
