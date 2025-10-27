4ee1001b9fa22db616469b8f52530a23971999dc
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-10-27 09:10:59 +0100

    remove lot of useless stuff

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -3,9 +3,7 @@ import "./styles/App.css";
 import Header from "./components/layout/Header.tsx";
 import PageFrame from "./components/layout/PageFrame.tsx";
 import Footer from "./components/layout/Footer.tsx";
-import { Outlet, useNavigate } from "react-router-dom";
-import Button from "./components/ui/Button.tsx";
-import { JSX } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
 	// useEffect(() => {
@@ -17,18 +15,6 @@ function App() {
 	//   socket.onmessage = (event) => console.log("Server:", event.data);
 	//   return () => socket.close();
 	// }, []);
-
-	const navigate = useNavigate();
-
-	const handleLogout = () => {
-		// Supprime le token
-		localStorage.removeItem("authToken");
-		// Redirect vers login
-		navigate("/auth/login");
-	};
-
-	// Verif si user est connecte
-	const isAuthenticated = localStorage.getItem("authToken") !== null;
 
 	return (
 		<main>
