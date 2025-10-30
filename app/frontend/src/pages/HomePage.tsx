24af6e571e1a0ae00dc7e8787973074921d6fffb
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-10-30 10:57:17 +0100

    test video

--- app/frontend/src/pages/HomePage.tsx
+++ app/frontend/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
 	return (
 		<div className="w-full h-full flex items-center justify-center">
 			<video
-				src="" // tu peux mettre n'importe quelle vidéo temporaire
+				src="/videos/screen2.mp4" // tu peux mettre n'importe quelle vidéo temporaire
 				autoPlay
 				loop
 				muted
