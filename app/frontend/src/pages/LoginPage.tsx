93fe6532ea9857b9426d510a6e5d71d2b957e444
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-10-30 09:32:55 +0100

    wrapper pour afficher la tele

--- app/frontend/src/pages/LoginPage.tsx
+++ app/frontend/src/pages/LoginPage.tsx
@@ -3,17 +3,19 @@ import LoginScreen from "../utils/LoginScreen.tsx";
 
 export default function LoginPage() {
 	return (
-		<RetroTvFrame
-			videoSrc="videos/screen2.mp4"
-			tvImageSrc="assets/TV.png"
-			tvWidth={1920}
-			tvHeight={1080}
-			screenX={400}
-			screenY={200}
-			screenWidth={1100}
-			screenHeight={700}
-		>
-			<LoginScreen />
-		</RetroTvFrame>
+		<div data-component="TVWrapper" className="relative w-[95vw] max-w-[95rem] aspect-square flex items-center justify-center my-4">
+			<RetroTvFrame
+				videoSrc="/videos/screen2.mp4"
+				tvImageSrc="/assets/TV.png"
+				tvWidth={1920}
+				tvHeight={1080}
+				screenX={400}
+				screenY={200}
+				screenWidth={1100}
+				screenHeight={700}
+			>
+				<LoginScreen />
+			</RetroTvFrame>
+		</div>
 	);
 }
