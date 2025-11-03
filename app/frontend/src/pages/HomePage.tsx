eaa1036241468b92ac20293660ddf85af344ec5b
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-11-03 13:51:54 +0100

    WIP thumbnails test

--- app/frontend/src/pages/HomePage.tsx
+++ app/frontend/src/pages/HomePage.tsx
@@ -1,15 +1,27 @@
 import styles from "./HomePage.module.css";
 
+import testThumbnail from "../../public/videos/screen2.mp4";
+
+const thumbnailsTest: Array<string> = Array(9).fill(testThumbnail);
+
 export default function HomePage() {
+	const listThumbnailsTest: JSX.Element[] = thumbnailsTest.map(
+		(path, index) => (
+			<li key={index}>
+				<video
+					src="/videos/screen2.mp4" // tu peux mettre n'importe quelle vidéo temporaire
+					autoPlay
+					loop
+					muted
+					className={styles.TMPVideo}
+				/>
+			</li>
+		),
+	);
+
 	return (
 		<div className={styles.tmp}>
-			<video
-				src="/videos/screen2.mp4" // tu peux mettre n'importe quelle vidéo temporaire
-				autoPlay
-				loop
-				muted
-				className={styles.TMPVideo}
-			/>
+			<ul className={styles.thumbnails}>{listThumbnailsTest}</ul>
 		</div>
 	);
 }
