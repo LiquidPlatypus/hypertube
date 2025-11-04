d8aa24ecc49ad55a82eb051feaff5a0cd0a86393
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-11-04 13:18:49 +0100

    thumbnails + layout test

--- app/frontend/src/pages/HomePage.tsx
+++ app/frontend/src/pages/HomePage.tsx
@@ -1,27 +1,38 @@
+import type { JSX } from "react";
+
 import styles from "./HomePage.module.css";
 
-import testThumbnail from "../../public/videos/screen2.mp4";
+import testThumbnail from "/assets/elementor-placeholder-image.png";
+import RetroTvFrame from "../components/TVFrame.tsx";
 
-const thumbnailsTest: Array<string> = Array(9).fill(testThumbnail);
+const thumbnailsTest: Array<string> = Array(20).fill(testThumbnail);
 
 export default function HomePage() {
 	const listThumbnailsTest: JSX.Element[] = thumbnailsTest.map(
 		(path, index) => (
 			<li key={index}>
-				<video
-					src="/videos/screen2.mp4" // tu peux mettre n'importe quelle vidéo temporaire
-					autoPlay
-					loop
-					muted
-					className={styles.TMPVideo}
-				/>
+				<img src={path} alt={`Thumbnail ${index + 1}`} />
 			</li>
 		),
 	);
 
 	return (
-		<div className={styles.tmp}>
-			<ul className={styles.thumbnails}>{listThumbnailsTest}</ul>
+		<div className={styles.TVWrapper}>
+			<RetroTvFrame
+				tvImageSrc="/assets/TV4.png"
+				videoSrc="/videos/screen2.mp4"
+				tvWidth={1920}
+				tvHeight={1080}
+				screenX={560}
+				screenY={245}
+				screenWidth={1100}
+				screenHeight={500}
+				contentScale={1}
+			>
+				<div className={styles.content}>
+					<ul className={styles.thumbnails}>{listThumbnailsTest}</ul>
+				</div>
+			</RetroTvFrame>
 		</div>
 	);
 }
