ac0b3d0b47097a8300174ff0e8e215132a0966d7
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-11-10 10:01:48 +0100

    background video

--- app/frontend/src/layout/MainLayout.tsx
+++ app/frontend/src/layout/MainLayout.tsx
@@ -18,6 +18,14 @@ export default function MainLayout({ children }: MainLayoutProps) {
 				"--footer-height": "100px",
 			} as React.CSSProperties}
 		>
+			<video
+				className={styles.BackgroundVideo}
+				src="/videos/screen2.mp4"
+				autoPlay
+				muted
+				loop
+				playsInline
+			/>
 			<header className={styles.MainHeader}>
 				<MainHeader />
 			</header>
