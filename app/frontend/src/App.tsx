1e72abd92c873ef9b2660a787130bd3ebeb27f7a
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-11-10 09:25:21 +0100

    correct layout on each pages

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import MainLayout from "./layout/MainLayout.tsx";
+import LoginLayout from "./layout/LoginLayout.tsx";
 
 function App() {
+	const { pathname } = useLocation();
+	const isAuthRoute = pathname.startsWith("/auth");
+
+	const Layout = isAuthRoute? LoginLayout : MainLayout;
+
 	return (
-		<div>
-			<MainLayout>
-				<Outlet />
-			</MainLayout>
-		</div>
+		<Layout>
+			<Outlet />
+		</Layout>
 	);
 }
 
