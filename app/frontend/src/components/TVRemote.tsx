d2798f3aff75dcebecdb9e49693d3234f1df7ec7
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-11-11 14:06:21 +0100

    buttons styling

--- app/frontend/src/components/TVRemote.tsx
+++ app/frontend/src/components/TVRemote.tsx
@@ -4,8 +4,19 @@ import Button from "./ui/Button.tsx";
 export default function TVRemote() {
 	return (
 		<div className={styles.TVRemote}>
-			<Button text="Search" size="small" shape="pill" className={styles.SearchBtn} />
-			<Button text="Logout" size="small" shape="pill" className={styles.LogoutBtn} />
+			<Button
+				text="Search"
+				size="small"
+				shape="pill"
+				style={{backgroundColor: "#000000", color: "#FFFFFF"}}
+				className={styles.SearchBtn}
+			/>
+			<Button
+				text="Logout"
+				size="small" shape="pill"
+				style={{backgroundColor: "#000000", color: "#FFFFFF"}}
+				className={styles.LogoutBtn}
+			/>
 		</div>
 	);
 }
