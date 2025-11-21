1c2cc44ca2722ebb893edf5954c4dcc057de4a5a
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2025-11-21 17:02:53 +0100

    worling language switcher

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,12 +1,30 @@
 import { Outlet, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import MainLayout from "./layout/MainLayout.tsx";
 import LoginLayout from "./layout/LoginLayout.tsx";
+import { initLanguage, loadLanguage } from "./lang/i18n.tsx";
 
 function App() {
 	const { pathname } = useLocation();
 	const isAuthRoute = pathname.startsWith("/auth");
+	const [isI18nReady, setIsI18nReady] = useState(false);
 
 	const Layout = isAuthRoute? LoginLayout : MainLayout;
+
+	useEffect(() => {
+		const initI18n = async () => {
+			const savedLang = initLanguage();
+			await loadLanguage(savedLang);
+			setIsI18nReady(true);
+		};
+
+		initI18n();
+	}, []);
+
+	if (!isI18nReady) {
+		return <div>Loading...</div>;
+	}
 
 	return (
 		<Layout>
