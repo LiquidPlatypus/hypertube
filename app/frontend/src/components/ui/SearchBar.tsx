85c35f9baa9619950ed57b01f3de1e093cd9b6bc
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2025-11-21 10:42:12 +0100

    placeholder styling & input showing

--- app/frontend/src/components/ui/SearchBar.tsx
+++ app/frontend/src/components/ui/SearchBar.tsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import Input from "./Input.tsx";
 
 import styles from "./SearchBar.module.css";
 
 export default function SearchBar() {
+	const [searchValue, setSearchValue] = useState("");
+
 	return (
 		<div>
 			<Input
 				type="text"
 				placeholder="Search"
-				size="medium"
+				size="large"
 				shape="square"
-				style={{borderRadius: "7px"}}
+				style={{ width: "30rem" }}
 				className={styles.SearchBar}
+				value={searchValue}
+				onChange={(e) => setSearchValue(e.target.value)}
 				required
 			/>
 		</div>
