5dc927f8864bc75b194e5dd5d136e29477cb0170
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2025-11-24 17:17:34 +0100

    new back

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -1,22 +1,14 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException, Depends
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordBearer
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-from pydantic import BaseModel, EmailStr
-from datetime import datetime, timedelta
-from jose import JWTError, jwt
 import os
+from fastapi import FastAPI, Depends, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from .auth import router as auth_router
+from .utils import verif_access_token
+from .users import router as users_router
 
 # INIT
 
 app = FastAPI()
-
-SECRET_KEY = "super_secret_key" # Ben faut proteger sa niveau sécurité sinon t'es pas gentil
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="access_token")
 
 app.add_middleware(
 	CORSMiddleware,
@@ -26,37 +18,14 @@ app.add_middleware(
 	allow_headers=["*"],
 )
 
-class Storage:
-	def __init__(self):
-		self.users = []
-		self.password = []
-
-	def add_user(self, username: str, email: str, password: str, firstname: str, lastname: str):
-
-		user = {"id": len(self.users) + 1, "username": username, "email": email, "firstname": firstname, "lastname": lastname}
-		self.users.append(user)
-
-		self.password.append({"user_id": user["id"], "password": password})
-		return user
-
-	def get_user_by_id(self, user_id: int):
-		for u in self.users:
-			if u["id"] == user_id:
-				return u
-		return None
-
-	def get_user_password(self, user_id: int):
-		for p in self.password:
-			if p["user_id"] == user_id:
-				return p["password"]
-		return None
-
-	def get_all_users(self):
-		return self.users
-
-storage = Storage()
-
-# CODE
+@app.middleware("http")
+async def verif_header(request: Request, call_next):
+	headers = request.scope['headers']
+	element = request.headers.get('sec-fetch-user')
+	if element is not None:
+		return JSONResponse(status_code=403, content={'reason': "Forbidden"})
+	response = await call_next(request)
+	return response
 
 # @app.websocket("/ws")
 # async def websocket_endpoint(ws: WebSocket):
@@ -68,71 +37,17 @@ storage = Storage()
 #     except WebSocketDisconnect:
 #         print(f"❌ Client left")
 
-class RegisterRequest(BaseModel):
-	username: str
-	password: str
-	email: EmailStr
-	firstName: str
-	lastName: str
 
+# ROUTER
 
-@app.post("/api/register")
-async def register(data: RegisterRequest):
-	users_list = storage.get_all_users()
-	for user in users_list:
-		if user["username"] == data.username:
-			return {"returnValue": False}
-	user = storage.add_user(data.username, data.email, data.password, data.firstName, data.lastName)
-	return {"returnValue": True}
-
-class LoginRequest(BaseModel):
-	username: str
-	password: str
-
-@app.post("/api/login")
-async def login(data: LoginRequest):
-	users_list = storage.get_all_users()
-	for user in users_list:
-		if user["username"] == data.username and storage.get_user_password(user["id"]) == data.password:
-			access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-			access_token = create_access_token(data={"sub": str(user["id"])}, expires_delta=access_token_expires)
-			return {"access_token": access_token, "token_type": "bearer"}
-
-	raise HTTPException(
-		status_code=401,
-		detail="Invalid username or password",
-	)
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-	to_encode = data.copy()
-	if expires_delta:
-		expire = datetime.utcnow() + expires_delta
-	else:
-		expire = datetime.utcnow() + timedelta(minutes=15)
-	to_encode.update({"exp": expire})
-	encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-	return encoded_jwt
-
-def verif_access_token(token: str = Depends(oauth2_scheme)):
-	try:
-		payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-		user = storage.get_user_by_id(int(payload["sub"]))
-		return user
-	except JWTError:
-		raise HTTPException(
-			status_code=401,
-			detail="Unauthorized"
-		)
+app.include_router(auth_router)
+app.include_router(users_router)
 
 @app.get("/api/verify-token/{token}")
 async def verify_user_token(token: str):
 	res = verif_access_token(token)
 	return {"message": "Token is valid"}
 
-@app.get("/api/me")
-async def read_user_me(current_user=Depends(verif_access_token)):
-    return {"user": current_user}
-
 @app.get("/api/hello")
-async def get_hello(current_user=Depends(verif_access_token)):
+async def get_hello():
 	return {"message": "Hello from FastAPI 👋"}
