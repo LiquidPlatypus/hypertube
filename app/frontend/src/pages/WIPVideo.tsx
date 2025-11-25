ceeaeeccf02e4ef8161b7d1b17ae59c1d6e1baa6
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-11-25 13:38:20 +0100

    start

--- app/frontend/src/pages/WIPVideo.tsx
+++ app/frontend/src/pages/WIPVideo.tsx
@@ -1,6 +1,18 @@
+import styles from "./WIPVideo.module.css";
+
 export default function WIPVideo() {
 	return (
-		<>
-		</>
+		<div className={styles.wrapper}>
+			<div className={styles.videoPart}>
+				<video
+					className={styles.BackgroundVideo}
+					src="/videos/screen2.mp4"
+					playsInline
+				/>
+			</div>
+			<div className={styles.commentsPart}>
+
+			</div>
+		</div>
 	);
 }
