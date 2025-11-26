7aa449fb51f4fde8a5f768997e2607e48eaf77c8
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2025-11-26 13:49:18 +0100

    NOT FINAL design

--- app/frontend/src/pages/WIPVideo.tsx
+++ app/frontend/src/pages/WIPVideo.tsx
@@ -1,10 +1,15 @@
+import { useTranslation } from "../hooks/useTranslation.tsx";
+
 import styles from "./WIPVideo.module.css";
 
 export default function WIPVideo() {
+	const { t } = useTranslation();
+
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.contentPart}>
 				<div className={styles.videoPart}>
+					// TODO : MESSAGE SI LA VIDÉO NE FONCTIONNE PAS
 					<video
 						className={styles.video}
 						src="/videos/screen2.mp4"
@@ -12,25 +17,38 @@ export default function WIPVideo() {
 					/>
 				</div>
 				<div className={styles.miscellaneousPart}>
-					<div>
+					<div className={styles.mainInfos}>
 						<h2>TITLE</h2>
-						<h3>Summary</h3>
-						<p>tres long resumetres long resumetres long resumetres long resumetres long resume</p>
+						<h3>{t("video.summary")}</h3>
+						<p className={styles.summary}>
+							tres long resumetres long resumetres long resumetres
+							long resumetres long resume tres long resumetres
+							long resumetres long resumetres long resumetres long
+							resume tres long resumetres long resumetres long
+							resumetres long resumetres long resume
+						</p>
 					</div>
-					<div>
+					<div className={styles.cast}>
 						<ul>
-							<li>casting</li>
+							<li>{t("video.casting")}</li>
 						</ul>
+					</div>
+					<div className={styles.meta}>
 						<p>year</p>
 						<p>lenght</p>
 						<p>grade</p>
 					</div>
-					<img src="/path/to/cover.jpg" alt="cover"/>
+					<div className={styles.cover}>
+						<img
+							src="/assets/Vertical_placeholder.svg"
+							alt="cover"
+						/>
+					</div>
 				</div>
 			</div>
 
 			<div className={styles.commentsPart}>
-				<h3>Comments</h3>
+				<h2>{t("video.comments")}</h2>
 				<p>fffffffffff</p>
 				<p>fffffffffff</p>
 				<p>fffffffffff</p>
