75a869a81a4963687ba96f89f16726df3dedb5d2
Emy <emy.nyc68@gmail.com>
2025-12-10 11:20:36 +0100

    Remote responsive sur mobile, changement de la video de WIPVideo :)

--- app/frontend/src/pages/WIPVideo.tsx
+++ app/frontend/src/pages/WIPVideo.tsx
@@ -1,20 +1,132 @@
+// import { useTranslation } from "../hooks/useTranslation.tsx";
+
+// import styles from "./WIPVideo.module.css";
+
+// const comments = [
+// 	{ id: 1, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 2, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 3, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 4, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 5, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 6, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 7, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 8, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 9, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 10, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 11, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 12, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 13, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 14, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 15, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 16, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 17, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 18, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 19, pseudo: "Pseudo", text: "fffffffffff" },
+// 	{ id: 20, pseudo: "Pseudo", text: "fffffffffff" },
+// ]
+
+// export default function WIPVideo() {
+// 	const { t } = useTranslation();
+
+// 	return (
+// 		<div className={styles.wrapper}>
+// 			<div className={styles.contentPart}>
+// 				<div className={styles.videoPart}>
+// 					<video
+// 						className={styles.video}
+// 						src="/videos/screen2.mp4"
+// 						controls
+// 					>
+// 						<p>{t("video.error")}</p>
+// 					</video>
+// 				</div>
+// 				<div className={styles.miscellaneousPart}>
+// 					<div className={styles.mainInfos}>
+// 						<h2>TITLE</h2>
+// 						<p className={styles.summary}>
+// 							tres long resumetres long resumetres long resumetres
+// 							long resumetres long resume tres long resumetres
+// 							long resumetres long resumetres long resumetres long
+// 							resume tres long resumetres long resumetres long
+// 							resumetres long resumetres long resume
+// 						</p>
+// 					</div>
+// 					<div className={styles.rightInfos}>
+// 						<div className={styles.meta}>
+// 							<p>year</p>
+// 							<p>lenght</p>
+// 							<p>grade</p>
+// 						</div>
+// 						<div className={styles.cover}>
+// 							<img
+// 								src="/assets/Vertical_placeholder.svg"
+// 								alt="cover"
+// 							/>
+// 						</div>
+// 					</div>
+// 				</div>
+
+// 				<div className={styles.cast}>
+// 					<h3>{t("video.casting")}</h3>
+// 					<ul className={styles.castList}>
+// 						<li><img
+// 							src="/assets/Vertical_placeholder.svg"
+// 							alt="casting"
+// 						/></li>
+// 						<li><img
+// 							src="/assets/Vertical_placeholder.svg"
+// 							alt="casting"
+// 						/></li>
+// 						<li><img
+// 							src="/assets/Vertical_placeholder.svg"
+// 							alt="casting"
+// 						/></li>
+// 						<li><img
+// 							src="/assets/Vertical_placeholder.svg"
+// 							alt="casting"
+// 						/></li>
+// 						<li><img
+// 							src="/assets/Vertical_placeholder.svg"
+// 							alt="casting"
+// 						/></li>
+// 						<li><img
+// 							src="/assets/Vertical_placeholder.svg"
+// 							alt="casting"
+// 						/></li>
+// 					</ul>
+// 				</div>
+// 			</div>
+
+// 			<div className={styles.commentsPart}>
+// 				{comments.map((comment) => (
+// 					<div key={comment.id} className={styles.comment}>
+// 						<h3>{comment.pseudo}</h3>
+// 						<p>{comment.text}</p>
+// 					</div>
+// 				))}
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+
 import { useTranslation } from "../hooks/useTranslation.tsx";
 
 import styles from "./WIPVideo.module.css";
 
 const comments = [
-	{ id: 1, pseudo: "Pseudo", text: "fffffffffff" },
+	{ id: 1, pseudo: "Pseudo", text: "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" },
 	{ id: 2, pseudo: "Pseudo", text: "fffffffffff" },
 	{ id: 3, pseudo: "Pseudo", text: "fffffffffff" },
 	{ id: 4, pseudo: "Pseudo", text: "fffffffffff" },
 	{ id: 5, pseudo: "Pseudo", text: "fffffffffff" },
 	{ id: 6, pseudo: "Pseudo", text: "fffffffffff" },
 	{ id: 7, pseudo: "Pseudo", text: "fffffffffff" },
-	{ id: 8, pseudo: "Pseudo", text: "fffffffffff" },
+	{ id: 8, pseudo: "Pseudo", text: "ffffffffffffffffffffffffff ffffffffffffffffffffffffffffff" },
 	{ id: 9, pseudo: "Pseudo", text: "fffffffffff" },
 	{ id: 10, pseudo: "Pseudo", text: "fffffffffff" },
 	{ id: 11, pseudo: "Pseudo", text: "fffffffffff" },
-	{ id: 12, pseudo: "Pseudo", text: "fffffffffff" },
+	{ id: 12, pseudo: "Pseudo", text: "ffffffffffffffffffffffffffffffffffffffffffffffffff" },
 	{ id: 13, pseudo: "Pseudo", text: "fffffffffff" },
 	{ id: 14, pseudo: "Pseudo", text: "fffffffffff" },
 	{ id: 15, pseudo: "Pseudo", text: "fffffffffff" },
@@ -34,7 +146,7 @@ export default function WIPVideo() {
 				<div className={styles.videoPart}>
 					<video
 						className={styles.video}
-						src="/videos/screen2.mp4"
+						src="/videos/santa.mp4"
 						controls
 					>
 						<p>{t("video.error")}</p>
@@ -42,7 +154,7 @@ export default function WIPVideo() {
 				</div>
 				<div className={styles.miscellaneousPart}>
 					<div className={styles.mainInfos}>
-						<h2>TITLE</h2>
+						<h2>{t("video.title")}</h2>
 						<p className={styles.summary}>
 							tres long resumetres long resumetres long resumetres
 							long resumetres long resume tres long resumetres
@@ -53,9 +165,9 @@ export default function WIPVideo() {
 					</div>
 					<div className={styles.rightInfos}>
 						<div className={styles.meta}>
-							<p>year</p>
-							<p>lenght</p>
-							<p>grade</p>
+							<p>{t("video.year")}</p>
+							<p>{t("video.length")}</p>
+							<p>{t("video.grade")}</p>
 						</div>
 						<div className={styles.cover}>
 							<img
@@ -69,41 +181,57 @@ export default function WIPVideo() {
 				<div className={styles.cast}>
 					<h3>{t("video.casting")}</h3>
 					<ul className={styles.castList}>
-						<li><img
-							src="/assets/Vertical_placeholder.svg"
-							alt="casting"
-						/></li>
-						<li><img
-							src="/assets/Vertical_placeholder.svg"
-							alt="casting"
-						/></li>
-						<li><img
-							src="/assets/Vertical_placeholder.svg"
-							alt="casting"
-						/></li>
-						<li><img
-							src="/assets/Vertical_placeholder.svg"
-							alt="casting"
-						/></li>
-						<li><img
-							src="/assets/Vertical_placeholder.svg"
-							alt="casting"
-						/></li>
-						<li><img
-							src="/assets/Vertical_placeholder.svg"
-							alt="casting"
-						/></li>
+						<li>
+							<img
+								src="/assets/Vertical_placeholder.svg"
+								alt="casting"
+							/>
+						</li>
+						<li>
+							<img
+								src="/assets/Vertical_placeholder.svg"
+								alt="casting"
+							/>
+						</li>
+						<li>
+							<img
+								src="/assets/Vertical_placeholder.svg"
+								alt="casting"
+							/>
+						</li>
+						<li>
+							<img
+								src="/assets/Vertical_placeholder.svg"
+								alt="casting"
+							/>
+						</li>
+						<li>
+							<img
+								src="/assets/Vertical_placeholder.svg"
+								alt="casting"
+							/>
+						</li>
+						<li>
+							<img
+								src="/assets/Vertical_placeholder.svg"
+								alt="casting"
+							/>
+						</li>
 					</ul>
 				</div>
 			</div>
 
 			<div className={styles.commentsPart}>
-				{comments.map((comment) => (
-					<div key={comment.id} className={styles.comment}>
-						<h3>{comment.pseudo}</h3>
-						<p>{comment.text}</p>
-					</div>
-				))}
+				<h2>{t("video.comments")}</h2>
+
+				<div className={styles.commentsList}>
+					{comments.map((comment) => (
+						<div key={comment.id} className={styles.comment}>
+							<h3>{comment.pseudo}</h3>
+							<p>{comment.text}</p>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
