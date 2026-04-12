7b1daadcd286efcd8b4e9efb770f35f95a08775a
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-04-12 09:47:01 +0200

    infinite scroll

--- app/frontend/src/pages/HomePage.tsx
+++ app/frontend/src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
+import * as React from "react";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useSearch } from "../utils/searchContext.tsx";
 
 import {useTranslation} from "../hooks/useTranslation.tsx";
@@ -20,63 +21,116 @@ export default function HomePage() {
 	const navigate = useNavigate();
 
 	const { searchTerm } = useSearch();
-	const [results, setResults] = useState<any[]>([]);
+	const [results, setResults] = useState<Movie[]>([]);
+	const [hasMore, setHasMore] = useState(true);
+	const [page, setPage] = useState(1);
 	const [loading, setLoading] = useState(false);
 	const [showLoader, setShowLoader] = useState(false);
 	const [error, setError] = useState<string | null>(null);
+	const observer = React.useRef<IntersectionObserver | null>(null);
 
 	const { t } = useTranslation();
 
+	// Affiche le loader uniquement si le chargement dure > 250ms (évite les flashs).
 	useEffect(() => {
 		let cancelled = false;
+		let loaderTimer: number | undefined;
 
-		// Show loader only if load time > 250ms.
-		setShowLoader(false);
-		const loaderTimer = window.setTimeout(() => {
-			if (!cancelled) setShowLoader(true);
-		}, 250);
-
-		const fetchData = async () => {
-			setLoading(true);
-			setError(null);
-
-			try {
-				const url = searchTerm
-					? `/api/thumbnails?query=${encodeURIComponent(searchTerm)}`
-					: `/api/thumbnails`;
-
-				const res = await fetch(url);
-				if (!res.ok) {
-					throw new Error (`HTTP ${res.status}`);
-				}
-
-				const data = (await res.json()) as Movie[];
-				if (!cancelled) setResults(data);
-			} catch (e) {
-				if (!cancelled) {
-					setResults([]);
-					setError(e instanceof Error ? e.message : t("error.unknow"));
-				}
-			} finally {
-				window.clearTimeout(loaderTimer);
-				if (!cancelled) {
-					setLoading(false);
-					setShowLoader(false);
-				}
-			}
-		};
-
-		fetchData();
+		if (loading) {
+			setShowLoader(false);
+			loaderTimer = window.setTimeout(() => {
+				if (!cancelled) setShowLoader(true);
+			}, 250);
+		} else {
+			setShowLoader(false);
+		}
 
 		return () => {
 			cancelled = true;
-			window.clearTimeout(loaderTimer);
+			if (loaderTimer) window.clearTimeout(loaderTimer);
+		};
+	}, [loading]);
+
+	const isFetchingRef = React.useRef(false);
+
+	const loadMovies = useCallback(async (query: string, pageNum: number, isNewSearch: boolean) => {
+		if (isFetchingRef.current) return;
+		isFetchingRef.current = true;
+		setLoading(true);
+
+		setError("");
+
+		try {
+			const url = query
+				? `/api/thumbnails?query=${encodeURIComponent(query)}&page=${pageNum}`
+				: `/api/thumbnails?page=${pageNum}`;
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+			const data: Movie[] = await response.json();
+			if (data.length === 0)
+				setHasMore(false);
+			else {
+				setResults(prev => {
+					const merged = isNewSearch ? data : [...prev, ...data];
+					return Array.from(new Map(merged.map(m => [m.id, m])).values());;
+				});
+				setHasMore(true);
+			}
+		} catch (err) {
+			setError("Erreur lors de la recherche de films. Le backend eest-il lancé ?");
+			console.error("Error fetching thumbnails:", err);
+		} finally {
+			setLoading(false);
+			isFetchingRef.current = false;
 		}
-	}, [searchTerm]);
+	}, []);
+
+	const observerReference = useCallback((node: HTMLLIElement | null) => {
+		if (!node) return;
+
+		observer.current?.disconnect();
+
+		observer.current = new IntersectionObserver(
+			entries => {
+				const first = entries[0];
+				if (first.isIntersecting && hasMore && !isFetchingRef.current) {
+					setPage(p => p + 1);
+				}
+			},
+			{ rootMargin: "200px 0px", threshold: 0 }
+		);
+
+		observer.current.observe(node);
+	}, [hasMore]);
+
+
+	const handleSearchBar = (e: React.FormEvent) => {
+		e.preventDefault();
+		setResults([]);
+		setPage(1);
+		setHasMore(true);
+	};
 
 	const handleThumbnailClick = (movieId: number) => {
 		navigate(`/movie/${movieId}`);
 	};
+
+	useEffect(() => {
+		loadMovies(searchTerm, page, page === 1);
+	}, [page, searchTerm, loadMovies]);
+
+	useEffect(() => {
+		return () => {
+			observer.current?.disconnect();
+		}
+	}, []);
 
 	return (
 		<div className={styles.content}>
@@ -89,18 +143,25 @@ export default function HomePage() {
 			{error && <div>{t("error")}{error}</div>}
 
 			<ul className={styles.thumbnails}>
-				{results.map((movie: Movie) => (
-					<li key={movie.id}>
-						<Thumbnail
-							thumbnailSrc={movie.poster_path}
-							thumbnailAlt={movie.title}
-							title={movie.title}
-							year={movie.release_date}
-							rating={movie.score}
-							onClick={() => handleThumbnailClick(movie.id)}
-						/>
-					</li>
-				))}
+				{results.map((movie: Movie, index: number) => {
+					const isLast = index === results.length - 1;
+
+					return (
+						<li
+							key={movie.id}
+							ref={isLast ? observerReference : null}
+						>
+							<Thumbnail
+								thumbnailSrc={movie.poster_path}
+								thumbnailAlt={movie.title}
+								title={movie.title}
+								year={movie.release_date}
+								rating={movie.score}
+								onClick={() => handleThumbnailClick(movie.id)}
+							/>
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);
