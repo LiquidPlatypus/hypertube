c8bfa747bcdfc413f76093a8556db0fe47a8bdeb
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-07-08 14:47:24 +0200

    Update router.tsx

--- app/frontend/src/router.tsx
+++ app/frontend/src/router.tsx
@@ -31,7 +31,7 @@ export const router = createBrowserRouter([
 						),
 					},
 					{
-						path: "/movie/:id",
+						path: "/movie/:archiveId",
 						element: (
 							<VideoPage />
 						),
